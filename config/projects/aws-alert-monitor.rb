f7db8238834068bedc4eac861fdd247cb5560e9d
Brett Weaver <brett@weav.net>
2013-02-05 14:42:13 -0800

    initial commit

---
+++ config/projects/aws-alert-monitor.rb
@@ -0,0 +1,9 @@
+name            "aws-alert-monitor"
+
+install_path    "/opt/intu/aws-alert-monitor"
+build_version   "0.0.5"
+build_iteration "1"
+
+dependencies    ["preparation", "aws-alert-monitor"]
+
+exclude         "\.git*"
---
+++ config/software/aws-alert-monitor.rb
@@ -0,0 +1,10 @@
+name "aws-alert-monitor"
+version "0.0.5"
+
+dependencies ["libxslt", "libxml2", "rubygems", "ruby"]
+
+env = { "GEM_HOME" => nil, "GEM_PATH" => nil }
+
+build do
+  gem "install aws-alert-monitor -n #{install_dir}/bin --no-rdoc --no-ri -v #{version}", :env => env
+end
