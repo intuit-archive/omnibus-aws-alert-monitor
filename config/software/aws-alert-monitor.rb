996ae801ae694133fc35d77b580856998eaa6cf3
Kevin Young <kevin_young@intuit.com>
2013-09-10 11:20:15 -0700

    updates

--- config/software/aws-alert-monitor.rb
+++ config/software/aws-alert-monitor.rb
@@ -1,7 +1,7 @@
 name "aws-alert-monitor"
 version ENV['version']
 
-dependencies ["rubygems"]
+dependencies ["libxslt", "libxml2", "rubygems", "ruby"]
 
 env = { "GEM_HOME" => nil, "GEM_PATH" => nil }
 
